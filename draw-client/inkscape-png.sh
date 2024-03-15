ca62c26290ea736d33879353183117feedcef9ab
Jan Ypma <jan@ypmania.nl>
2024-03-15 17:31:42 +0100

    Add a little script that will create PNGs with background color

---
+++ draw-client/inkscape-png.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Exports the given SVG file to PNG, 300 DPI, preserving the contained background color.
+set -e
+
+FILE=$1
+COLOR=$(grep pagecolor exported.svg |  sed 's/^.*pagecolor="\([^"]*\)".*$/\1/')
+BASE="${FILE%.svg}"
+
+inkscape $FILE -o $BASE.png -d 300 -b $COLOR
